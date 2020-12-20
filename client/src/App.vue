13d0aae85a28b3d65233bef6254339ae6eb6c8e5
RomainGuarinoni <romain.guar91@gmail.com>
2020-12-20 22:22:12 +0100

    layout first page vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,90 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="container">
+      <div class="titre">
+        <p><span class="underline">CHOISISSEZ</span> VOTRE SALLE</p>
+      </div>
+      <div class="salleCard">
+        <SalleCard
+          index="1"
+          name="Descartes"
+          status="libre"
+          img="'../assets/Descartes.jpg'"
+        />
+        <SalleCard
+          index="2"
+          name="Lavoisier"
+          status="Prise"
+          img="'../assets/Lavoisier.jpg'"
+        />
+      </div>
+      <div class="info"><p>?</p></div>
+    </div>
   </div>
 </template>
 
+<script>
+import SalleCard from "./components/SalleCard";
+export default {
+  components: {
+    SalleCard,
+  },
+};
+</script>
 <style>
 #app {
-  font-family: "Roboto", sans-serif;
+  font-family: "Open Sans", sans-serif;
   height: 100%;
+  width: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 }
 * {
   margin: 0;
   padding: 0;
 }
-::-webkit-scrollbar {
-  width: 2px;
-  background: white;
+.container {
+  width: 700px;
 }
-::-webkit-scrollbar-thumb {
-  background: black;
+.info {
+  width: 25px;
+  height: 25px;
+  background: rgba(212, 211, 211, 0.521);
+  border: none;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all ease 200ms;
+  cursor: pointer;
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+}
+.info:hover {
+  transform: scale(1.2);
+}
+.titre {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  margin-bottom: 50px;
+}
+.titre p {
+  font-weight: bold;
+  font-size: 40px;
+}
+.underline {
+  border-bottom: 5px solid black;
+}
+.salleCard {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
 }
 </style>
---
+++ client/src/components/SalleCard.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    id="card"
+    :style="{
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.575), rgba(0, 0, 0, 0.329)),url(${
+        imgTab[index - 1]
+      })`,
+    }"
+  >
+    <div class="header">
+      <div class="salleInfo">
+        <p class="salleNumber">Salle {{ index }}</p>
+        <p class="name">{{ name }}</p>
+      </div>
+      <div id="bouton">
+        <i class="fas fa-chevron-right"></i>
+      </div>
+    </div>
+    <div class="status">
+      <div class="statustxt">
+        <p>{{ status }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import img1 from "../assets/Descartes.jpg";
+import img2 from "../assets/Lavoisier.jpg";
+export default {
+  props: ["index", "name", "status", "img"],
+  data() {
+    return {
+      imgTab: [img1, img2],
+    };
+  },
+  mounted: function() {
+    console.log(this.img);
+  },
+};
+</script>
+
+<style scoped>
+#card {
+  margin: 10px;
+  height: 320px;
+  color: white;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  border-radius: 10px;
+  transition: all ease 200ms;
+  background-size: 200%;
+  background-position: center;
+  background-repeat: no-repeat;
+  cursor: pointer;
+}
+#card:hover {
+  box-shadow: 5px 5px 5px rgba(54, 54, 54, 0.623);
+}
+#card:hover #bouton {
+  transform: translate(10px);
+}
+#card:hover #bouton i {
+  transform: translate(2px);
+}
+i {
+  transition: all ease 200ms;
+}
+.header {
+  display: flex;
+  height: 80px;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
+#bouton {
+  width: 28px;
+  height: 28px;
+  color: black;
+  border-radius: 100%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all ease 200ms;
+}
+.status {
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
+.statustxt {
+  margin: 10px;
+}
+.name {
+  font-weight: bold;
+  font-size: 25px;
+}
+</style>
